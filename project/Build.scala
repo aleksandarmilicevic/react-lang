d4df823ddcafe380b1bdd8b1c5f0c760c518cf56
Damien Zufferey <zufferey@csail.mit.edu>
2015-06-01 13:02:35 -0400

    updating dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object BuildSettings {
     version := buildVersion,
     scalaVersion := buildScalaVersion,
     resolvers += "Local ROS Java maven repo" at "file://"+baseDir+"/rosjava-maven",
-    resolvers += "ROS Java maven repo" at "https://github.com/stonier/rosjava_mvn_repo/raw/master",
+    resolvers += "ROS Java maven repo" at "https://github.com/rosjava/rosjava_mvn_repo/raw/master",
     resolvers += "dzufferey maven repo" at "https://github.com/dzufferey/my_mvn_repo/raw/master/repository",
     unmanagedBase := new java.io.File(baseDir + "/lib"),
     parallelExecution in Test := false,
@@ -38,15 +38,15 @@ object MyBuild extends Build {
     settings = buildSettings ++ Seq(
       libraryDependencies ++= Seq(
           "org.scala-lang" % "scala-reflect" % scalaVersion.value,
-          "org.scalatest" %% "scalatest" % "2.2.1" % "test",
+          "org.scalatest" %% "scalatest" % "2.2.4" % "test",
           /* ROS dependencies */
-          "org.ros.rosjava_bootstrap" % "message_generation" % "0.2.0",
-          "org.ros.rosjava_core" % "rosjava" % "0.2.0",
+          "org.ros.rosjava_bootstrap" % "message_generation" % "0.2.1",
+          "org.ros.rosjava_core" % "rosjava" % "0.2.1",
           "org.ros.rosjava_messages" % "std_msgs"  % "0.5.9",
-          "org.ros.rosjava_messages" % "geometry_msgs" % "1.11.4",
-          "org.ros.rosjava_messages" % "sensor_msgs" % "1.11.4",
-          "org.ros.rosjava_messages" % "nav_msgs" % "1.11.4",
-          "org.ros.rosjava_messages" % "gazebo_msgs" % "2.3.4",
+          "org.ros.rosjava_messages" % "geometry_msgs" % "1.11.7",
+          "org.ros.rosjava_messages" % "sensor_msgs" % "1.11.7",
+          "org.ros.rosjava_messages" % "nav_msgs" % "1.11.7",
+          "org.ros.rosjava_messages" % "gazebo_msgs" % "2.4.7",
           /* Other dependencies */
           "org.scream3r" % "jssc" % "2.8.0",
           "io.github.dzufferey" %% "misc-scala-utils" % "0.1-SNAPSHOT"
@@ -60,22 +60,23 @@ object MyBuild extends Build {
     file("verifier"),
     settings = buildSettings ++ Seq(
       libraryDependencies ++= Seq(
-          "org.scalatest" %% "scalatest" % "2.2.1" % "test",
+          "org.scalatest" %% "scalatest" % "2.2.4" % "test",
           "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.3",
           /* ROS dependencies */
-          "org.ros.rosjava_core" % "rosjava" % "0.2.0",
+          "org.ros.rosjava_core" % "rosjava" % "0.2.1",
           /* Other dependencies */
           "org.apache.commons" % "commons-lang3" % "3.2.1",
           "commons-codec" % "commons-codec" % "1.10",
-          "net.automatalib" % "automata-parent" % "0.4.1",
-          "net.automatalib.archetypes" % "automata-archetypes-parent" % "0.4.1",
-          "net.automatalib.distribution" % "automata-distribution" % "0.4.1",
-          "net.automatalib" % "automata-adapters-parent" % "0.4.1",
-          "net.automatalib" % "automata-api" % "0.4.1",
-          "net.automatalib" % "automata-commons-parent" % "0.4.1",
-          "net.automatalib" % "automata-core" % "0.4.1",
-          "net.automatalib" % "automata-misc-parent" % "0.4.1",
-          "net.automatalib" % "automata-util" % "0.4.1",
+          "net.automatalib" % "automata-parent" % "0.5.2",
+          "net.automatalib" % "automata-api" % "0.5.2",
+          "net.automatalib" % "automata-core" % "0.5.2",
+          "net.automatalib" % "automata-util" % "0.5.2",
+          "net.automatalib" % "automata-incremental" % "0.5.2",
+          "net.automatalib" % "automata-misc-algorithms" % "0.5.2",
+          "net.automatalib" % "automata-brics" % "0.5.2",
+          "net.automatalib" % "automata-commons-util" % "0.5.2",
+          "net.automatalib" % "automata-commons-smartcollections" % "0.5.2",
+          "net.automatalib" % "automata-commons-dotutil" % "0.5.2",
           "io.github.dzufferey" %% "misc-scala-utils" % "0.1-SNAPSHOT",
           "io.github.dzufferey" %% "scala-smtlib-interface" % "0.1-SNAPSHOT"
       )
@@ -88,11 +89,11 @@ object MyBuild extends Build {
     settings = buildSettings ++ Seq(
       libraryDependencies ++= Seq(
         /* ROS dependencies */
-        "org.ros.rosjava_core" % "rosjava" % "0.2.0",
+        "org.ros.rosjava_core" % "rosjava" % "0.2.1",
         "org.ros.rosjava_messages" % "std_msgs" % "0.5.9",
-        "org.ros.rosjava_messages" % "geometry_msgs" % "1.11.4",
-        "org.ros.rosjava_messages" % "sensor_msgs" % "1.11.4",
-        "org.ros.rosjava_messages" % "turtlesim" % "0.4.2",
+        "org.ros.rosjava_messages" % "geometry_msgs" % "1.11.7",
+        "org.ros.rosjava_messages" % "sensor_msgs" % "1.11.7",
+        "org.ros.rosjava_messages" % "turtlesim" % "0.5.2",
         "io.github.dzufferey" %% "scala-arg" % "0.1-SNAPSHOT",
         "io.github.dzufferey" %% "misc-scala-utils" % "0.1-SNAPSHOT"
       )
