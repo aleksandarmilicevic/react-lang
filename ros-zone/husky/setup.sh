334ec9c4adbb7b4a28b4c927752acc8c66aaec40
Damien Zufferey <zufferey@csail.mit.edu>
2015-07-31 16:31:42 -0400

    husky part needs to be updated

--- ros-zone/husky/setup.sh
+++ ros-zone/husky/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo TODO this is outdated
+exit -1
+
 echo getting the husky dependencies ...
 wstool init src -j5 https://github.com/husky/install/raw/master/desktop.rosinstall
 rosdep install --from-paths src -i -y
